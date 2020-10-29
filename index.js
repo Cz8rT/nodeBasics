b87c887e0194361079c0a17ebbb92adf49bc435c
Adam Welnicki <adam.welnicki@gmail.com>
2020-10-29 16:25:21 +0100

    3. Added script(start) in package.json and initiated basic server routes

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get("/", (request, response) => {
+    response.send("Hello in main route!");
+})
+
+app.get("/test", (request, response) => {
+    response.send("Witaj w '/test'!");
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
