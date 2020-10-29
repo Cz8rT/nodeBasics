06182fa893a8ad308998233ff7a2328507ea960e
Adam Welnicki <adam.welnicki@gmail.com>
2020-10-29 18:30:59 +0100

    6. Added new endpoints and error handlers in index.js

--- index.js
+++ index.js
@@ -13,8 +13,70 @@ app.get("/status", (request, response) => {
 });
 
 app.post("/rejestracja", (request, response) => {
-    console.log("Post... in '/rejstracja'");
+    console.log(request.body);
+    if(!request.body) {
+        response.status(400).json({message: "invalid body", status: "400"})
+    } else {
+        response.status(200).json({ message: "ok", status: 200});
+    }
+});
+
+app.post("/login", (request, response) => {
+    console.log("Post... in '/login'");
+    if(!request.body) {
+        response.status(400).json({message: "invalid body", status: "400"})
+    } else {
+        response.status(200).json({ message: "ok", status: 200});
+    }
+});
+
+app.post("/logout", (request, response) => {
+    console.log("Post... in '/logout'");
+    if(!request.body) {
+        response.status(400).json({message: "invalid body", status: "400"})
+    } else {
+        response.status(200).json({ message: "ok", status: 200});
+    }
+});
+
+app.post("/token", (request, response) => {
+    console.log("Post... in '/token'");
+    if(!request.body || !request.body.refreshToken) {
+        response.status(400).json({message: "invalid body", status: "400"})
+    } else {
+        const { refreshToken } = request.body;
+        response.status(200).json({ 
+            message: `Refresh token requested for token: ${refreshToken}`, status: 200
+        });
+    }
+});
+
+app.post("/forgot-password", (request, response) => {
+    console.log("Post... in '/forgot-password'");
+    if(!request.body || !request.body.email) {
+        response.status(400).json({message: "invalid body", status: "400"})
+    } else {
+        const { email } = request.body;
+        response.status(200).json({ 
+            message: `Forgot password requested for email: ${email}`, status: 200
+        });
+    }
+});
+
+app.post("/reset-password", (request, response) => {
+    console.log("Post... in '/reset-password'");
     response.status(200).json({ message: "ok", status: 200});
+});
+
+// catch all other routes
+app.use((request, response) => {
+    response.status(404).json({message: "404 - Not found", status: 404});
+});
+
+// handle errors 
+app.use((error, request, response, next) => {
+    console.log(error);
+    response.status(error.status || 500).json({error: error.message, status: 500});
 });
 
 app.listen(port, () => {
