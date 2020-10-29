0013bba58be8969c4454473b4c31e6439f8784a9
Adam Welnicki <adam.welnicki@gmail.com>
2020-10-29 17:35:44 +0100

    5. Added GET status and POST rejestracja endpoints

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ const app = express();
 const port = 3000;
 
 app.get("/", (request, response) => {
-    console.log("Get...");
+    console.log("Get... in '/'");
     response.send("Hello in main route!");
-})
+});
 
-app.get("/test", (request, response) => {
-    response.send("Witaj w '/test'!");
-})
+app.get("/status", (request, response) => {
+    console.log("Get... in '/status'");
+    response.status(200).json({ message: "ok", status: 200});
+});
+
+app.post("/rejestracja", (request, response) => {
+    console.log("Post... in '/rejstracja'");
+    response.status(200).json({ message: "ok", status: 200});
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
